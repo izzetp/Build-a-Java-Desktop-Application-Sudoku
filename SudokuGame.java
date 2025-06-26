55be959a9254fe864bdb06cac25069d9334671f2
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-26 09:47:22 +0200

    Create SudokuGame.java

---
+++ SudokuGame.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDRY = 9;
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    Public GameState gameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SudokuUtilies.copyToNewArray(gridState);
+    }
+}
