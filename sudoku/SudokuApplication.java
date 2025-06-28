a9a0fffcfc01d2884255648cc0a2287a8c2146b4
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-28 14:25:49 +0200

    Update SudokuApplication.java

--- sudoku/SudokuApplication.java
+++ sudoku/SudokuApplication.java
@@ -1,5 +1,29 @@
 package sudoku;
 
-public class SudokuApplication {
-    
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FMXLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SudokuApplication extends Application {
+    private UserInterfaceContract.View uiImpl;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+
+
+    public static void main(String[] args) { 
+        launch(args); 
+    }
 }
