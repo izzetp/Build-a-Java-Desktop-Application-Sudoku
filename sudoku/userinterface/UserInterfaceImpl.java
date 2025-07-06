61088b2b671b1ec1aefc98cbbec88fd85c3c0a75
izzetp <135380394+izzetp@users.noreply.github.com>
2025-07-06 16:01:20 +0200

    Update UserInterfaceImpl.java

--- sudoku/userinterface/UserInterfaceImpl.java
+++ sudoku/userinterface/UserInterfaceImpl.java
@@ -14,10 +14,15 @@ public class UserInterfaceImpl implements IUserInterfaceContract.view,
 
     private IUserInterfaceContract.EventListener listener;
 
-    @Override
-    public void handle(KeyEvent keyEvent) {
+    private static final double WINDOW_X = 732;
+    private static final double WINDOW_Y = 668;
+    private static final double BOARD_PADDING = 50;
+    private static final double BOARD_X_AND_Y = 576;
 
-    }
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(0, 150, 135);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224, 242, 241);
+    private static final String SUDOKU = "Sudoku";
+
 
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
@@ -26,6 +31,26 @@ public class UserInterfaceImpl implements IUserInterfaceContract.view,
 
     @Override
      public void updateSquare(int x, int y , int input) {
+
+    }
+
+    @Override
+    public void updateBoard(SudokuGame game) {
+
+    }
+
+    @Override
+    public void showDialog(String Message) {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
 
     }
 }
