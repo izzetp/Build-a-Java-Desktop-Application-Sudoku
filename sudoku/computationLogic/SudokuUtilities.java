ec45c9360d2a443702639ff36d3be21107377c7f
izzetp <135380394+izzetp@users.noreply.github.com>
2025-07-07 14:53:32 +0200

    Create SudokuUtilities.java

---
+++ sudoku/computationLogic/SudokuUtilities.java
@@ -0,0 +1,11 @@
+package sudoku.computationLogic;
+
+public class SudokuUtilities {
+    public static void copySudokuArrayValues(int[][] oldArray, int[][] newArray) {
+        for (int xIndex = 0; xIndex < SudokuGame.GRID_BOUNDARY; xIndex++) {
+            for (int yIndex = 0; yIndex < SudokuGame.GRID_BOUNDARY; yIndex++) {
+            
+            }
+        }
+    }
+}
